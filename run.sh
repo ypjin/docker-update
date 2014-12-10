280fd992430ff682bdc618c0792d017547bdeb77
tifayuki <tifayuki@gmail.com>
2014-12-10 20:40:11 +0800

    download sig file

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -e
 
 DOCKER_DIR="/usr/lib/tutum/"
 DOCKER_FILENAME="docker.new"
-
+DOCKER_SIG_FILENAME="docker.new.sig"
 if [ "${DOCKER_VERSION}" == "**ChangeMe**" ]; then
     echo "=> Need to specify docker verion using DOCKER_VERSION enviornment variable. Exit(1)"
     exit 1
@@ -13,13 +13,19 @@ fi
 echo "=> Downloading docker version ${DOCKER_VERSION}"
 curl -OL https://files.tutum.co/packages/docker/docker-${DOCKER_VERSION}
 
+echo "=> Downloading docker signature"
+curl -OL https://files.tutum.co/packages/docker/docker-${DOCKER_VERSION}.sig
+
 echo "=> Checking MD5 of docker binary"
 curl -L https://files.tutum.co/packages/docker/docker-${DOCKER_VERSION}.md5 | md5sum -c -
 echo "=> MD5 matched"
 
-echo "=> Copy docker binary to ${DOCKER_DIR}"
 mkdir -p ${DOCKER_DIR}
 mv docker-${DOCKER_VERSION} ${DOCKER_FILENAME}
+mv docker-${DOCKER_VERSION}.sig ${DOCKER_SIG_FILENAME}
 chmod +x ${DOCKER_FILENAME}
+echo "=> Copy docker binary to ${DOCKER_DIR}"
 cp -f ${DOCKER_FILENAME} ${DOCKER_DIR}
+echo "=> Copy docker binary signature to ${DOCKER_DIR}"
+cp -f ${DOCKER_SIG_FILENAME} ${DOCKER_DIR}
 echo "=> Done"
